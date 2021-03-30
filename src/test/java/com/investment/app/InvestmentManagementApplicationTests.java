3b4b3d27b3db7ff5448f8be6f29d258ae3bfded3
Rahul <sinha.r@northeastern.edu>
2021-03-30 19:28:59 -0400

    First commit

---
+++ src/test/java/com/investment/app/InvestmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investment.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
